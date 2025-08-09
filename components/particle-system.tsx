69dd67662c126146188e022759f60407e54ee4e8
BigZorin <zorin@hotmail.nl>
2025-08-09 09:45:50 +0200

    fix: initialize refs

--- components/particle-system.tsx
+++ components/particle-system.tsx
@@ -25,7 +25,7 @@ export function ParticleSystem({
 }: ParticleSystemProps) {
   const canvasRef = useRef<HTMLCanvasElement>(null)
   const particlesRef = useRef<Particle[]>([])
-  const animationRef = useRef<number>()
+  const animationRef = useRef<number | null>(null)
 
   useEffect(() => {
     const canvas = canvasRef.current
